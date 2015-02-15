23654190963cd14ebf1f4b9d7034929ca8f7a45f
vitaliy <krasylovets@gmail.com>
2015-02-15 14:17:22 -0500

    Created Apple Script

---
+++ Assets/Scripts/KillBellow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBellow : MonoBehaviour {
+
+	public float die_at = -10f;
+
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < this.die_at) {
+			Destroy(this.gameObject);
+		}
+	}
+}
