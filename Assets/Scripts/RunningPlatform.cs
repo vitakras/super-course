e2bacf26e8f2e0cd6ab0aacd00102c4c73ab61cd
vitaliy <krasylovets@gmail.com>
2015-03-02 15:22:04 -0500

    Walking

---
+++ Assets/Scripts/RunningPlatform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunningPlatform : MonoBehaviour {
+
+	public PlayerController player;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player.KinectMove (Vector3.forward);
+	}
+}
