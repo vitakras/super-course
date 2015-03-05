06fcf41d1e95130bb544e1e663b17d557db6c0a6
vitaliy <krasylovets@gmail.com>
2015-03-04 19:32:39 -0500

    Modifying controller to work with CharecterMotor

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+
+	private CharacterMotor motor;
+
+	// Use this for initialization
+	void Start () {
+		motor = GetComponent <CharacterMotor> ();
+		//motor.inputJump = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		motor.inputJump = Input.GetButton("Jump");
+	}
+}
