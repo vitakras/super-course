e442abd67a39b580c0276be70399fee15f9d4578
vitaliy <krasylovets@gmail.com>
2015-03-04 20:27:31 -0500

    Follow Path class

---
+++ Assets/MarkerPath.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarkerPath : MonoBehaviour {
+
+	public GameObject[] markers;
+
+	private int index;
+
+	// Use this for initialization
+	void Start () {
+		this.index = 0;
+	}
+
+	/* Returns the Directions of the Next marker or Zero vector is at last marker */
+	public Vector3 NextMarkerDirection(Vector3 playerPosition) {
+		if(index >= this.markers.Length) {
+			return Vector3.zero;
+		}
+
+		Vector3 heading = markers[index].transform.position - playerPosition;
+
+		if ((((int)heading.x) == 0) && (((int)heading.z) == 0)) {
+			index++;
+			if(index >= this.markers.Length) {
+				return Vector3.zero;
+			} else {
+				heading = markers[index].transform.position - playerPosition;
+			}
+		}
+
+		float distance = heading.magnitude;
+
+		return heading / distance;
+	}
+}
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class Test : MonoBehaviour {
 
+	public MarkerPath path;
 
-	private CharacterMotor motor;
+	//private CharacterMotor motor;
+
 
 	// Use this for initialization
 	void Start () {
-		motor = GetComponent <CharacterMotor> ();
+		///motor = GetComponent <CharacterMotor> ();
 		//motor.inputJump = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		motor.inputJump = Input.GetButton("Jump");
+		Vector3 direction = (path.NextMarkerDirection(transform.position));
+		transform.Translate(direction * 5.0f * Time.deltaTime);
+		//motor.inputMoveDirection = transform.rotation * direction;
+		//motor.inputJump = Input.GetButton("Jump");
 	}
 }
