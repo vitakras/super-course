086ec90ca4e4db08a99a47ecb47d80eba6f9b7c7
shgfl007 <shgfl@hotmail.com>
2015-03-08 00:26:56 -0500

    added a open scene before game demo added a new terrain

---
+++ Assets/Scripts/InitTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitTrigger : MonoBehaviour {
+
+	public GameObject trigger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float Seconds = 59;
+	public float Minutes = 0;
+	public GameObject prefab;
+	int count = 0;
+	Vector3 playerPos;
+	public GameObject player;
+	Vector3 prefabPos;
+	Quaternion rotation;
+	//public PlayerController player;
+
+	// Update is called once per frame
+	void Update () {
+		if(Seconds <= 0)
+		{
+			Seconds = 59;
+			if(Minutes >= 1)
+			{
+				Minutes--;
+			}
+			else
+			{
+				Minutes = 0;
+				Seconds = 0;
+				// This makes the guiText show the time as X:XX. ToString.("f0") formats it so there is no decimal place.
+			}
+		}
+		else
+		{
+			Seconds -= Time.deltaTime;
+		}
+
+		if (Seconds == 0 && Minutes == 0 && count == 0) {
+			//player = GameObject.FindGameObjectsWithTag("Player");
+			playerPos = player.transform.position;
+			prefabPos = playerPos;
+			prefabPos.x = prefabPos.x + 10;
+			prefabPos.z = prefabPos.z + 2;
+			rotation = Quaternion.identity;
+			rotation.y = 90;
+			Instantiate(prefab,prefabPos, Quaternion.identity);
+			count++;
+		}
+	}
+}
