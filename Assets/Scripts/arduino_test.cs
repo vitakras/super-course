71c4f1e466ec2f16027e2e72b7ec38b794081ae4
shgfl007 <shgfl@hotmail.com>
2015-03-17 10:29:05 -0400

    arduino pulse sensor added

--- Assets/Scripts/arduino_test.cs
+++ Assets/Scripts/arduino_test.cs
@@ -9,23 +9,39 @@ public class arduino_test : MonoBehaviour {
 
 	SerialPort sp = new SerialPort("/dev/tty.usbmodem1411", 9600);
 
+	public Light RedLight;
+	public Light BlueLight;
+	public int Number = 1;
 
 	// Use this for initialization
 	void Start () {
 		sp.Open ();
 		sp.ReadTimeout = 1;
+
+		Number = 1;
+		BlueLight.GetComponent<Light>().intensity = 0;
+		RedLight.GetComponent<Light>().intensity = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		amountToMove = speed * Time.deltaTime;
+		//amountToMove = speed * Time.deltaTime;
 
 		if (sp.IsOpen) 
 		{
 			try
 			{
-				MoveObject(sp.ReadByte());
+				//MoveObject(sp.ReadByte());
 				print(sp.ReadByte());
+				if(sp.ReadByte() == 2)
+				{
+					flashing();
+				}
+				else
+				{
+					BlueLight.GetComponent<Light>().intensity = 0;
+					RedLight.GetComponent<Light>().intensity = 0;
+				}
 			}catch (System.Exception)
 			{
 
@@ -35,13 +51,40 @@ public class arduino_test : MonoBehaviour {
 
 	void MoveObject(int Direction)
 	{
-		if (Direction == 1) 
+		/*if (Direction == 1) 
 		{
 			transform.Translate(Vector3.left * amountToMove, Space.World);
 		}
 		if (Direction == 2) 
 		{
 			transform.Translate(Vector3.right * amountToMove, Space.World);
+		}*/
+	}
+
+	void flashing()
+	{
+		if (Number == 1)
+		{
+			BlueLight.GetComponent<Light>().intensity = 0;
+			RedLight.GetComponent<Light>().intensity = 1.5f;
+			StartCoroutine(waitforred());
 		}
+		if (Number == 2)
+		{
+			RedLight.GetComponent<Light>().intensity = 0;
+			BlueLight.GetComponent<Light>().intensity = 1.5f;
+			StartCoroutine(waitforblue());
+		}
+	}
+
+	IEnumerator waitforred()
+	{
+		yield return new WaitForSeconds (0.2f);
+		Number = 2;
+	}
+	IEnumerator waitforblue()
+	{
+		yield return new WaitForSeconds (0.2f);
+		Number = 1;
 	}
 }
