332cb03c3b869ef91e377372c701fff2b8ecad54
vitaliy <krasylovets@gmail.com>
2015-03-26 09:49:17 -0400

    no message

--- Assets/Scripts/MainMenu/PauseMenuHandler.cs
+++ Assets/Scripts/MainMenu/PauseMenuHandler.cs
@@ -7,6 +7,7 @@ public class PauseMenuHandler : MonoBehaviour {
 	public GameObject gameMenu;
 
 	private static PauseMenuHandler _instance;
+	private InteractionManager interactionManager;
 
 	public static PauseMenuHandler Instance {
 		get
@@ -25,7 +26,8 @@ public class PauseMenuHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		interactionManager = InteractionManager.Instance;
+		this.interactionManager.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -40,12 +42,14 @@ public class PauseMenuHandler : MonoBehaviour {
 	}
 
 	public void Resume() {
+		this.interactionManager.enabled = false;
 		pauseMenu.SetActive(false);
 		gameMenu.SetActive(true);
 		GameStateManager.Instance.ResumeGame();
 	}
 
 	public void Pause() {
+		this.interactionManager.enabled = true;
 		GameStateManager.Instance.PauseGame();
 		pauseMenu.SetActive(true);
 		gameMenu.SetActive(false);
